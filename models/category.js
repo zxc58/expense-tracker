6419fd78d7e2c527964dee1e09c66e9cf5cc5377
zxc58 <abc01234567889@gmail.com>
2022-04-02 21:53:04 +0800

    設定好model

--- models/category.js
+++ models/category.js
@@ -1,6 +1,9 @@
 //
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const categorySchema = new Schema({})
+const categorySchema = new Schema({
+    name:{type:String,required:true},
+    url:{type:String}
+})
 //
 module.exports = mongoose.model('Category', categorySchema)
