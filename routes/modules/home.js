42238923221b20117c644113b805922a90a01820
zxc58 <abc01234567889@gmail.com>
2022-04-02 20:36:37 +0800

    設計路由

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,5 +2,8 @@
 const express = require('express')
 const router = express.Router()
 //
+router.get("/",(req,res)=>{
+    //r
+})
 //
 module.exports = router
--- routes/modules/record.js
+++ routes/modules/record.js
@@ -2,5 +2,20 @@
 const express = require('express')
 const router = express.Router()
 //
+router.get("/",(req,res)=>{
+    //c
+})
+router.post("/",(req,res)=>{
+    //c
+})
+router.get("/:_id",(req,res)=>{
+    //u
+})
+router.put("/:_id",(req,res)=>{
+    //u
+})
+router.delete("/:_id",(req,res)=>{
+    //d
+})
 //
 module.exports = router
