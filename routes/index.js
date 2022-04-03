8cea2a14b8bdf19e10d72beb82c6fad53aec9f40
zxc58 <abc01234567889@gmail.com>
2022-04-03 15:20:54 +0800

    認證登入狀態

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 //
 const express = require('express')
 const router = express.Router()
-const user = require("./modules/user")
-const record = require("./modules/record")
-const home=require("./modules/home")
+const user = require('./modules/user')
+const record = require('./modules/record')
+const home = require('./modules/home')
+const { authenticator } = require('../middleWare')
 //
-router.use("/user",user)
-router.use("/recoed",record)
-router.use("/home",home)
+router.use('/user', user)
+router.use('/record', authenticator, record)
+router.use('/home', authenticator, home)
 //
 module.exports = router
