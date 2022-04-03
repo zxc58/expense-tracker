b8acfd21d4af13fab7d14a1e14adf888192a9198
zxc58 <abc01234567889@gmail.com>
2022-04-03 15:18:25 +0800

    "認證登入狀態"中介函數,

--- middleWare.js
+++ middleWare.js
@@ -3,9 +3,16 @@
 //
 module.exports = {
   validationGuard: (req, res, next) => {
-    const {body:{name,amount,date,categoryId}}= req
-    //這邊驗證資料格式是否正確
-    req.newRecord={name,amount,date,categoryId,}//還有userId
+    const userId = req.user._id
+    const { body: { name, amount, date, categoryId } } = req
+    // 這邊驗證資料格式是否正確
+    req.newRecord = { name, amount, date, categoryId, userId }
     next()
+  },
+  authenticator: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/user/signin')
   }
 }
