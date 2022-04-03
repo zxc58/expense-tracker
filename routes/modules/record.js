625c8cc6e7765dd429f322590a0d5d486421322e
zxc58 <abc01234567889@gmail.com>
2022-04-03 14:04:57 +0800

    新增record路由處理(未完成)

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -1,21 +1,53 @@
 //
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/category')
+const User = require('../../models/user')
+const Record = require('../../models/record')
+const { validationGuard } = require('../../middleWare')
 //
-router.get("/",(req,res)=>{
-    //c
+router.get('/new', (req, res) => {
+  res.render('new')
 })
-router.post("/",(req,res)=>{
-    //c
+router.post('/', validationGuard, async (req, res) => {
+  // c
+  try {
+    const result = await Record.create(req.newRecord)
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
 })
-router.get("/:_id",(req,res)=>{
-    //u
+router.get('/:_id/edit', async (req, res) => {
+  // u
+  try {
+    const recordId = req.params._id
+    const searchResults = await Record.findById(recordId).lean()
+    res.render('edit', { searchResults })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
 })
-router.put("/:_id",(req,res)=>{
-    //u
+router.put('/:_id', validationGuard, async (req, res) => {
+  // u
+  try {
+    const recordId = req.params._id
+    const result = await Record.findByIdAndUpdate(recordId, req.newRecord)
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
 })
-router.delete("/:_id",(req,res)=>{
-    //d
+router.delete('/:_id', async (req, res) => {
+  // d
+  try {
+    const recordId = req.params._id
+    const result = await Record.findByIdAndDelete(recordId)
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
 })
 //
 module.exports = router
