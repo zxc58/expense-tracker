058f37b8c271ccec98cedea27e5c812fcb7fdac4
zxc58 <abc01234567889@gmail.com>
2022-04-03 22:02:35 +0800

    新增user(登入登出註冊)處理函數

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,21 +1,35 @@
 //
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
+const { userValidationGuard } = require('../../middleWare')
+const Category = require('../../models/category')
+const User = require('../../models/user')
+const Record = require('../../models/record')
 //
-router.get("/signin",(req,res)=>{
-    
+router.get('/signin', (req, res) => {
+  res.send("sign in page")
 })
-router.post("/signin",(req,res)=>{
-    
+router.post('/signin', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/user/signin' }))
+router.get('/signup', (req, res) => {
+  res.send("sign up page")
 })
-router.get("/signup",(req,res)=>{
+router.post('/signup', userValidationGuard, async (req, res) => {
+  try {
+    const searchResult =  await User.findOne({name:req.newUser.name})
+    if(searchResult)
+      return res.send("name has sign up")
+    await User.create(req.newUser)
+    return res.redirect('/')
     
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
 })
-router.post("/signup",(req,res)=>{
-    
-})
-router.get("/signout",(req,res)=>{
-    
+router.get('/signout', (req, res) => {
+  req.logout()
+  res.redirect("/")
 })
 //
 module.exports = router
