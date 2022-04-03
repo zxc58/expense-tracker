5723eff653db5a8b92e2eaec64ae44b2421cff0c
zxc58 <abc01234567889@gmail.com>
2022-04-03 14:03:00 +0800

    新增首頁路由處理

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,9 +1,20 @@
 //
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/category')
+const User = require('../../models/user')
+const Record = require('../../models/record')
 //
-router.get("/",(req,res)=>{
-    //r
+router.get('/', async (req, res) => {
+  // r
+  try {
+    const userId = req.user._id
+    const searchResults = await Record.find({ userId }).lean()//
+    res.render('home', { searchResults })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
 })
 //
 module.exports = router
