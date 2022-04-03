433d1da1eaabe9414fc4f03276560ec07088a95b
zxc58 <abc01234567889@gmail.com>
2022-04-03 14:00:39 +0800

    引入method-override  修正bug

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 //
 require('dotenv').config()
-require("./config/mongoose")
+require('./config/mongoose')
 const express = require('express')
 const session = require('express-session')
 const app = express()
 const port = parseInt(process.env.port)
 const router = require('./routes/index')
+const methodOverride = require('method-override')
 //
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized:false
+  saveUninitialized: false
 }))
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use('/', router)
 app.listen(port, () => {
