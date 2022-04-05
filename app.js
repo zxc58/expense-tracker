31d2c426f00a08d37712812e5be18e9592839b18
zxc58 <abc01234567889@gmail.com>
2022-04-05 17:06:59 +0800

    初始化express handlebars

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ require('dotenv').config()
 require('./config/mongoose')
 const express = require('express')
 const session = require('express-session')
+const methodOverride = require('method-override')
+const {engine} = require("express-handlebars")
 const usePassport = require('./config/passport')
 const router = require('./routes/index')
-const methodOverride = require('method-override')
+
 const app = express()
 const port = parseInt(process.env.port)
 //
@@ -17,6 +19,9 @@ app.use(session({
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 app.use('/', router)
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
