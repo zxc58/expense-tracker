87bfbe83db520e1c70f0c8b4086843974ef61e2d
zxc58 <abc01234567889@gmail.com>
2022-04-07 06:10:44 +0800

    完成首頁的篩選功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,18 +2,23 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
-/*這行不能刪*/const Category =require("../../models/category")
+const Category = require('../../models/category')
 //
 router.get('/', async (req, res) => {
   // r
   try {
     const queryCondition = { userId: req.user._id }
-    if (req.query.filterByCategory&&req.query.filterByCategory!=="全部") { queryCondition.categoryId = req.query.filterByCategory }
+    if(!req.query.filterByCategory)req.query.filterByCategory="all"
+    const categoryList = await Category.find().lean().sort({ _id: 1 })
+    for(let category of categoryList){
+      if(category.name===req.query.filterByCategory){
+        category.selected='selected'
+        queryCondition.categoryId=category._id
+      }
+    }
     const searchResults = await Record.find(queryCondition).populate(['userId', 'categoryId']).lean().sort({ date: -1 })
-    const categoryList = await Category.find().lean().sort({_id:1})
     res.locals.searchResults = searchResults
-    res.locals.categoryList=categoryList
-    res.locals.user = req.user
+    res.locals.categoryList = categoryList
     res.render('home')
   } catch (error) {
     console.log(error)
