7e7c7479764dbe9371c71795c3c33057c9ccb042
zxc58 <abc01234567889@gmail.com>
2022-04-08 15:03:02 +0800

    加入totalAmount

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,19 +8,22 @@ const {dateTransform} = require('../../myFunction')
 router.get('/', async (req, res) => {
   // r
   try {
-    const queryCondition = { userId: req.user._id }
+    const findCondition = { userId: req.user._id }
+    let totalAmount=0
     if (!req.query.filterByCategory)req.query.filterByCategory = 'all'
     const categoryList = await Category.find().lean().sort({ _id: 1 })
     for (const category of categoryList) {
       if (category.name === req.query.filterByCategory) {
         category.selected = 'selected'
-        queryCondition.categoryId = category._id
+        findCondition.categoryId = category._id
       } else { category.selected = '' }
     }
-    const recordList = await Record.find(queryCondition).populate(['userId', 'categoryId']).lean().sort({ date: -1 })
+    const recordList = await Record.find(findCondition).populate(['userId', 'categoryId']).lean().sort({ date: -1 })
     for(const record of recordList){
       record.date=dateTransform(record.date)
+      totalAmount+=record.amount
     }
+    res.locals.totalAmount=totalAmount
     res.locals.searchResults = recordList
     res.locals.categoryList = categoryList
     res.render('home')
